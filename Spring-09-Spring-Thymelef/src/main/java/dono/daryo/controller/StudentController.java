d360a42741e27cc7e1d7942707681d2273737630
SHOKHZODJURAEV <85317197+SHOKHZODJURAEV@users.noreply.github.com>
2023-05-21 16:28:18 -0400

    Thymeleaf 09

---
+++ Spring-09-Spring-Thymelef/src/main/java/dono/daryo/controller/StudentController.java
@@ -0,0 +1,39 @@
+package dono.daryo.controller;
+
+import dono.daryo.bootstrap.DataGenerator;
+
+
+import dono.daryo.enums.Gender;
+import dono.daryo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+    @RequestMapping("/register")
+    public String register(Model model){
+        model.addAttribute("students", DataGenerator.createStudent());
+        return "student/register";
+    }
+    @RequestMapping("/welcome")
+    public String info(){
+        return "student/welcome";
+    }
+
+    @RequestMapping("/list")
+    public String showTable(Model model){
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Shawn","Juraev", 30, Gender.Male ));
+        mentorList.add(new Mentor("Sabby","Singh", 30, Gender.Male ));
+        mentorList.add(new Mentor("Krit","Shah", 30, Gender.Male ));
+
+        model.addAttribute("mentors", mentorList);
+        return "student/mentor";
+    }
+}
